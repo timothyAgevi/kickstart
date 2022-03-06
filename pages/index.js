0c3974a149e2288b27c607f7b348c68ed451f446
timothyAgevi <timothyagevi@gmail.com>
2022-03-06 17:27:08 +0300

    add pseudocode for campaign list Page

--- pages/index.js
+++ pages/index.js
@@ -4,3 +4,8 @@ import React from 'react'
 export default ()=>{
     return <h1> This is the campaign list paage!!</h1>
 };
+//todos for campaign list page
+//1 configure web3 with provider from metamask
+// 2tell web3that deployed copy of 'campaign factory 'exists
+// 3.use factory instance to retrieve a list of deployed campaigns
+// use react to show sth abt each camIGN.   
